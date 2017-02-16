2a71821f08bdada47148a1d37154c8540a9c8abf
Ntthrk_win <freedomsoza@gmail.com>
2017-02-16 15:00:36 +0700

    Initial commit

---
+++ app/src/main/java/com/finalproject/ntthrk_win/psj_demo/TTSFragment.java
@@ -0,0 +1,69 @@
+package com.finalproject.ntthrk_win.psj_demo;
+
+
+import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.Locale;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class TTSFragment extends Fragment{
+    ArrayAdapter<String> adapter;
+    String[] txtList = {"ไปเชียงใหม่กันนะ", "ไปยะลาดีกว่า", "สวัสดีประเทศไทย",
+            "ทดสอบระบบ", "หิวข้าวจัง", "ง่วงนอนแล้วนะ", "ปั่นงานวนไป" ,"ผัดกะเพรา 3 ถุง" ,"ยายมี ขายหอย ยายมอย ขายหมี"};
+    ListView listView;
+
+
+    public TTSFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_tt, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        listView = (ListView) view.findViewById(R.id.listText);
+        adapter = new ArrayAdapter<String>(getContext(),
+                android.R.layout.simple_list_item_1, txtList);
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+
+
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getContext(),txtList[position],Toast.LENGTH_LONG).show();
+
+                MyTTS.getInstance(getContext())
+//                        .setEngine("com.google.android.tts")​
+                        .setLocale(new Locale("th"))
+                        .speak(txtList[position]);
+
+            }
+        });
+    }
+
+
+}
+
